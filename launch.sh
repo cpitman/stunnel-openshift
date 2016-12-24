5917c72646ce6290d233865f21f3bdff3188fd96
Chris Pitman <cpitman@redhat.com>
2016-12-23 22:57:13 -0500

    Initial docker artifacts

---
+++ launch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ ! -f /etc/stunnel/pki/cert.pem ]; then
+    openssl genrsa -out /etc/stunnel/pki/key.pem 2048
+    openssl req -new -x509 -key /etc/stunnel/pki/key.pem -out /etc/stunnel/pki/cert.pem -days 1095
+fi
+
+if [ -z CONNECT_PORT ]; then
+    sed -i -e "s/5001/${CONNECT_PORT}/" /etc/stunnel/config/config
+fi
+
+stunnel /etc/stunnel/config/config
