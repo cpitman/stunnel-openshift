20170f1a1ce652e1dda4e598fd686ec977303d7f
Chris Pitman <cpitman@redhat.com>
2016-12-24 00:36:32 -0500

    Make openssl run non-interactive

--- launch.sh
+++ launch.sh
@@ -2,7 +2,7 @@
 
 if [ ! -f /etc/stunnel/pki/cert.pem ]; then
     openssl genrsa -out /etc/stunnel/pki/key.pem 2048
-    openssl req -new -x509 -key /etc/stunnel/pki/key.pem -out /etc/stunnel/pki/cert.pem -days 1095
+    openssl req -new -x509 -key /etc/stunnel/pki/key.pem -out /etc/stunnel/pki/cert.pem -days 1095 -subj "/C=XX/CN=cpitman.github.io"
 fi
 
 if [ -z CONNECT_PORT ]; then
